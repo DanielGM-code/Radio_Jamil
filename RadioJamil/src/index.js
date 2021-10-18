036ce68ddfb49e99928ed769a926af28b839c16a
Seth Díaz <seth261099@gmail.com>
2021-10-18 15:07:54 -0500

    Corrección navegabilidad

--- RadioJamil/src/index.js
+++ RadioJamil/src/index.js
@@ -9,7 +9,7 @@ const createWindow = () => {
     fullscreenable: true,
     webPreferences: {
       nodeIntegration: true,
-      devTools: false
+      nativeWindowOpen: false,
     }
   });
 
@@ -17,15 +17,17 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'views/index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
+  // mainWindow.openDevTools();
   mainWindow.setMenu(null)
 };
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
+
 app.on('ready', createWindow);
+// app.whenReady().then(createWindow);
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
