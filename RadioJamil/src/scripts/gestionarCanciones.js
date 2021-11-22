190eb985ad3a4f48d576cc5d312b4f818dbb0d0c
Daniel Morales <dagamo0605@gmail.com>
2021-11-21 21:05:37 -0600

    Corrección de css en gestionar canciones
    Co-Authored-By: Seth Díaz <33437473+Sethndd@users.noreply.github.com>

--- RadioJamil/src/scripts/gestionarCanciones.js
+++ RadioJamil/src/scripts/gestionarCanciones.js
@@ -1,30 +1,12 @@
-//Esto va para otro archivo encargado de hacer peticiones
-async function pedirCanciones(){
-    const respuesta = await fetch('http://192.168.1.65:2610/canciones');
-    const jsonCanciones = await respuesta.json();
-    
-    return jsonCanciones
-}
-
-async function pedirCancion(id){
-    const respuesta = await fetch(`http://192.168.1.65:2610/canciones/${id}`);
-    const jsonCanciones = await respuesta.json();
-    
-    return jsonCanciones
-}
-
-//Codigo
 function cargarItems(items) {
     const cuerpoTabla = document.getElementById("tablaCuerpo");
     items.forEach( item => {
         var fila = cuerpoTabla.insertRow();
 
         var nombre = fila.insertCell(0);
-        nombre.innerHTML = item.idCancion;
-        var nombre = fila.insertCell(1);
         nombre.innerHTML = item.nombreCancion;
-        var artista = fila.insertCell(2);
-        artista.innerHTML = item.idArtista;
+        var artista = fila.insertCell(1);
+        artista.innerHTML = item.nombreArtista;
 
         fila.setAttribute('onclick',`clickFila(${item.idCancion})`)
     })
@@ -38,9 +20,9 @@ function clickFila(idCancion){
 
     pedirCancion(idCancion).then(cancion => {
         txtCancion.value = cancion.nombreCancion
-        txtArtista.value = cancion.idArtista
-        txtGenero.value = cancion.idGenero
-        txtCategoria.value = cancion.idCategoria
+        txtArtista.value = cancion.artista.nombre
+        txtGenero.value = cancion.nombreGenero
+        txtCategoria.value = cancion.nombreCategoria
     })
 }
 
