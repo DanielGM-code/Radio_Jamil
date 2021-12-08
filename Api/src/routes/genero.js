e0fb0e59f6073abf3d37ead5066b72197305990d
damianmendoza <61212902+damianmendoza@users.noreply.github.com>
2021-12-07 23:46:14 -0600

    rutas genero

--- Api/src/routes/genero.js
+++ Api/src/routes/genero.js
@@ -2,4 +2,36 @@ const express = require("express");
 const path = require('path');
 const router = express.Router();
 
+const generoDAO = require(path.join(path.resolve(__dirname, '..'), 'dataAccess', 'generoDAO.js'))
+
+router.route('/generos')
+    .get((req, res) =>{ 
+        generoDAO.obtenerTodos((err, respuesta) =>{ 
+            if(err){
+                console.log(err)
+                res.status(400).json(err)
+                return
+            }
+            res.status(200).json(respuesta)
+        })
+    })
+
+router.route('/generos/:id')
+    .get((req, res) =>{
+        generoDAO.obtener(req.params.id,(err, respuesta) =>{
+            if(err){
+                console.log(err)
+                res.status(400).json(err)
+                return
+            }
+            if(respuesta){
+                res.status(200).json(respuesta)
+            }
+            else{
+                res.status(404).json({Mensaje : 'genero no encontrada'})
+            }
+
+        })
+    })
+
 module.exports = router
