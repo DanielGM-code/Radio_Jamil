a5b8866143d80c696146d3faaad16a60e0213988
Abner Jeffrey Tapia Cruz <61332521+DrearyStudent@users.noreply.github.com>
2021-12-08 07:26:38 -0600

    Añadidos IDs

--- RadioJamil/src/scripts/autocompletar.js
+++ RadioJamil/src/scripts/autocompletar.js
@@ -1,13 +1,13 @@
-function autocompletar(array, buscar, listaHTML){
+function autocompletar(array, buscar, listaHTML, tipo){
     listaHTML.innerHTML = ''
     var seleccionados = []  
 
     if(buscar){
-        seleccionados = array.filter(elemento => elemento.toLowerCase().includes(buscar.toLowerCase()))
+        seleccionados = array.filter(elemento => elemento.nombre.toLowerCase().includes(buscar.toLowerCase()))
     }
 
     if(seleccionados.length > 0){
-        seleccionados.map(item => listaHTML.innerHTML += `<p class='contenido-dropdown'>${item}</p>`)
+        seleccionados.map(item => listaHTML.innerHTML += `<p class="contenido-dropdown" onclick="${tipo}=${item.id}">${item.nombre}</p>`)
         listaHTML.style.display = 'block'
     }
 }
--- RadioJamil/src/scripts/gestionarCanciones.js
+++ RadioJamil/src/scripts/gestionarCanciones.js
@@ -1,3 +1,5 @@
+const txtBuscador = document.getElementById('textFieldBuscador')
+
 const txtCancion = document.getElementById('textFieldCancion')
 const resultadosArtista = document.getElementById('resultadosArtista')
 const txtArtista = document.getElementById('textFieldArtista')
@@ -6,12 +8,18 @@ const txtGenero = document.getElementById('textFieldGenero')
 const resultadosCategoria = document.getElementById('resultadosCategoria')
 const txtCategoria = document.getElementById('textFieldCategoria')
 const cuerpoTabla = document.getElementById('tablaCuerpo')
+
 const btnCancelar = document.getElementById('buttonCancelar')
 const btnAceptar = document.getElementById('buttonRegistrar')
 
 var esRegistro = true
+var idArtista = 0;
+var idGenero = 0;
+var idCategoria = 0;
 
 function cargarItems(items) {
+    cuerpoTabla.innerHTML = ''
+
     items.forEach( item => {
         var fila = cuerpoTabla.insertRow();
 
@@ -38,20 +46,43 @@ function clickFila(idCancion){
     })
 }
 
+function buscarEnTabla(array, buscar){
+    seleccionados = []
+
+    if(buscar){
+        seleccionados = array.filter(cancion => 
+            cancion.nombre.toLowerCase().includes(buscar.toLowerCase()) 
+            ||
+            cancion.nombreArtista.toLowerCase().includes(buscar.toLowerCase()) 
+        )
+    }
+
+    if(seleccionados.length > 0){ 
+        console.log(seleccionados)
+        cargarItems(seleccionados)
+    }
+    else{
+        cargarItems(array)
+    }
+}
+
 //Se ejecuta al cargar la página
 window.onload = () =>{
     pedirCanciones().then(canciones => {
         cargarItems(canciones)
+        txtBuscador.addEventListener('input', evento =>{
+            buscarEnTabla(canciones, evento.target.value)
+        })
     })
 
-    pedirArtistas().then(artistas => {
+    pedirArtistas()/*.then(artistas => {
         return artistas.map(artista => {
             return artista.nombre
         })
-    })
+    })*/
     .then(artistas => {
         txtArtista.addEventListener('input', evento =>{
-            autocompletar(artistas, evento.target.value, resultadosArtista)
+            autocompletar(artistas, evento.target.value, resultadosArtista, 'idArtista')
         })
 
         resultadosArtista.addEventListener('click', evento => {
@@ -59,14 +90,14 @@ window.onload = () =>{
         })  
     }) 
     
-    pedirGeneros().then(generos => {
+    pedirGeneros()/*.then(generos => {
         return generos.map(genero => {
             return genero.nombre
         })
-    })
+    })*/
     .then(generos => {
         txtGenero.addEventListener('input', evento =>{
-            autocompletar(generos, evento.target.value, resultadosGenero)
+            autocompletar(generos, evento.target.value, resultadosGenero, 'idGenero')
         })
 
         resultadosGenero.addEventListener('click', evento => {
@@ -74,14 +105,14 @@ window.onload = () =>{
         })  
     }) 
 
-    pedirCategorias().then(categorias => {
+    pedirCategorias()/*.then(categorias => {
         return categorias.map(categorias => {
             return categorias.nombre
         })
-    })
+    })*/
     .then(categorias => {
         txtCategoria.addEventListener('input', evento =>{
-            autocompletar(categorias, evento.target.value, resultadosCategoria)
+            autocompletar(categorias, evento.target.value, resultadosCategoria, 'idCategoria')
         })
 
         resultadosCategoria.addEventListener('click', evento => {
@@ -108,5 +139,8 @@ window.onload = () =>{
         else{
             console.log('Estoy editando')
         }
+        console.log(idArtista)
+        console.log(idGenero)
+        console.log(idCategoria)
     })
 }
